56227a458157d62ecdbe7400ccb5781e3b7b3471
Kiattisak Khamchaitep <tom131320131320@gmail.com>
2019-07-13 14:14:28 +0700

    เพิ่มหน้าเทส

--- index.js
+++ index.js
@@ -6,5 +6,8 @@ const port = 4000
 app.get('/', (req, res) => res.send('Tom'))
 app.get('/about', (req, res) => res.send('หน้า about'))
 app.get('/contrack', (req, res) => res.send('ติดต่อเรา'))
+app.get('/test', (req, res) => {
+    res.json({message : "test"})
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
